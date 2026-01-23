e0cfdbdb84c88701bc299f13ea33f59fe44cfab2
Shapel Nahyrych <shapel.nahyrych@gmail.com>
2026-01-23 07:51:19 +0000

    practice-23-01-2026

--- src/components/Heros/HeroCard.jsx
+++ src/components/Heros/HeroCard.jsx
@@ -1,16 +1,30 @@
-import styles from "./Hero.module.css"
+import styles from "./Hero.module.css";
 
 const HeroCard = (props) => {
-  const { hero , setSelectHero } = props;
-  const { id, name, power } = hero;
-  const handleClick = ()=>{
-    setSelectHero(hero)
-  }
+  const { hero, setSelectHero, updateRating } = props;
+  const { id, name, power, rating } = hero;
+  const handleClick = () => {
+    setSelectHero(hero);
+  };
+  const incRating = (event) => {
+    event.stopPropagation();
+    updateRating(id, rating + 1 > 5 ? 5 : rating + 1);
+  };
+  const decRating = (event) => {
+    event.stopPropagation();
+    updateRating(id, rating - 1 < 1 ? 1 : rating - 1);
+  };
   return (
     <article onClick={handleClick} className={styles.hero}>
-      <h3>{name}</h3>
-      <p>power:{power}</p>
-    </article> 
+      <h3 className={styles.name}>{name}</h3>
+      <div className={styles.raw}>
+        <p>power:{power}</p>
+        <p>
+          {rating} <button onClick={incRating}>👍</button>{" "}
+          <button onClick={decRating}>👎</button>{" "}
+        </p>
+      </div>
+    </article>
   );
 };
 
--- src/components/Heros/HeroList.jsx
+++ src/components/Heros/HeroList.jsx
@@ -2,23 +2,39 @@ import { useState } from "react";
 import Heading from "../Heading/Heading";
 import { heroes } from "./data";
 import HeroCard from "./HeroCard";
-import styles from "./Hero.module.css"
+import styles from "./Hero.module.css";
 
 const HeroList = () => {
-    const [selectHero, setSelectHero] = useState(null);
-    const showHero = (hero)=><HeroCard key={hero.id} hero={hero} setSelectHero={setSelectHero} />
-    return (
-        <div>
-           <Heading text='pick favourite hero'/> 
-           <section className={styles.heroes}>
-            {heroes.map(showHero)}
-           </section>
-           <Heading text='you picked' />
-           <section>
-           { selectHero && <HeroCard hero={selectHero} />} 
-           </section>
-        </div>
+  const [heroesAr, setHeroesAr] = useState(heroes);
+  const [selectHero, setSelectHero] = useState(null);
+  const updateRating = (id, rating) => {
+    setHeroesAr((prevHeroes) =>
+      prevHeroes.map((hero) => (hero.id === id ? { ...hero, rating } : hero)),
     );
-}
+    if (selectHero && selectHero.id === id) {
+      setSelectHero((prevHero) => ({ ...prevHero, rating }));
+    }
+  };
+  const showHero = (hero) => (
+    <HeroCard
+      key={hero.id}
+      updateRating={updateRating}
+      hero={hero}
+      setSelectHero={setSelectHero}
+    />
+  );
+  return (
+    <div>
+      <Heading text="pick favourite hero" />
+      <section className={styles.heroes}>{heroesAr.map(showHero)}</section>
+      <Heading text="you picked" />
+      <section>
+        {selectHero && (
+          <HeroCard hero={selectHero} updateRating={updateRating} />
+        )}
+      </section>
+    </div>
+  );
+};
 
 export default HeroList;
