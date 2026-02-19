e2dd6c1ed807c7a80c4499c3fb5c60c21a46e4cb
Shapel Nahyrych <shapel.nahyrych@gmail.com>
2026-02-19 14:54:45 +0000

    practice-19-02-2026

--- src/components/UploadFile/UploadFile.jsx
+++ src/components/UploadFile/UploadFile.jsx
@@ -1,19 +1,47 @@
-import { useState } from 'react';
-import styles from './UploadFile.module.scss' ;
+import { useState, useEffect } from "react";
+import styles from "./UploadFile.module.scss";
 
 const UploadFile = () => {
-    const [progress, setProgress] = useState(0);
-    return (
-        <article className={styles.upload}>
-            <h2>Download file</h2>
-            <div className={styles.progressMain}>
-                <div className={styles.progress}></div>
-            </div>
-            <p>Progress {progress}%</p>
-            <button>Download file</button>
-            <p className={styles.success}>file was successfully downloaded</p>
-        </article>
-    );
-}
+  const [isDownload, setIsDownload] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  useEffect(() => {
+    if (isUploading === false) {
+      return;
+    }
+    const interval = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress >= 100) {
+          clearInterval(interval);
+          setIsDownload(true);
+          setIsUploading(false)
+          return 100;
+        }
+        return prevProgress + 2;
+      });
+    }, 200);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isUploading]);
+  const startDowload = () => {
+    setProgress(0);
+    setIsDownload(false);
+    setIsUploading(true);
+  };
+  return (
+    <article className={styles.upload}>
+      <h2>Download file</h2>
+      <div className={styles.progressMain}>
+        <div className={styles.progress} style={{width:`${progress}%`}}></div>
+      </div>
+      <p>Progress {progress}%</p>
+      <button onClick={startDowload} disabled={isUploading}>{isUploading?'Is uploading':'Download file'}</button>
+      {isDownload && (
+        <p className={styles.success}>file was successfully downloaded</p>
+      )}
+    </article>
+  );
+};
 
 export default UploadFile;
